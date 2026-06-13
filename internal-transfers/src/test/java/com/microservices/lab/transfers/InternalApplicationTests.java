b4d67e01858713a1724f88839a7e100c4b3dba44
Rafael Solano Martinez <rafaelsolanomartinez@Mac.lan>
2026-06-13 18:39:49 -0400

    Created internal-transfer module

---
+++ internal-transfers/src/test/java/com/microservices/lab/transfers/InternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.lab.transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
